2be38c1d100721e59175f0a990024ea91bb090ad
Humza Khalil <humzak21@vt.edu>
2025-09-18 01:18:56 -0400

    deployment fix 1

--- public/env.js
+++
@@ -1,7 +0,0 @@
-// Runtime environment variables for Railway
-// This file will be served statically and can be generated at deployment time
-window.__ENV__ = {
-  VITE_SUPABASE_URL: '{{VITE_SUPABASE_URL}}',
-  VITE_SUPABASE_ANON_KEY: '{{VITE_SUPABASE_ANON_KEY}}',
-  VITE_ADMIN_USER_ID: '{{VITE_ADMIN_USER_ID}}'
-};
